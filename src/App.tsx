f905b2a2800e7f6194248a0cff4121395707a17a
matheuscaetano-dev <matheus10.c.r@gmail.com>
2026-01-19 17:26:29 -0300

    feat: adiciona o componente de categorias

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Header } from "./components/Header"
 import { Hero } from "./components/Hero"
+import { Categories } from "./components/Categories"
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <main className="py-10">
       <Hero />
+      <Categories />
       </main>
     </>
   )
