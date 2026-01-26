a6100fe718fb630fad39f709da271a191afc2a68
matheuscaetano-dev <matheus10.c.r@gmail.com>
2026-01-26 13:07:44 -0300

    feat: adiciona listagem de produtos

--- src/pages/_app/products/index.tsx
+++ src/pages/_app/products/index.tsx
@@ -1,9 +1,19 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
+import { ProductList } from "../../../components/ProductList";
+import { products } from "../../../mocks/products";
 
-export const Route = createFileRoute('/_app/products/')({
+export const Route = createFileRoute("/_app/products/")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/products/"!</div>
+  return (
+    <div className="container pt-44 md:pt-54 pb-10 md:px-10 mb-10 text-black bg-surface">
+      <h1 className=" text-3xl text-center mb-3">Lista de produtos</h1>
+
+      <h2 className="text-center mb-10 p-4">Conforto expecional para suas aventuras do dia-a-dia</h2>
+
+      <ProductList products={products} />
+    </div>
+  );
 }
